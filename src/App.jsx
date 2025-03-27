fb5da3150b5ecc6d027aa9abed77b230fcdcfd74
Imanol Peralta <imanolaugusto18@gmail.com>
2025-03-27 20:39:23 -0300

    Corrigiendo basename para despliegue en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <>
+    <Router basename="/The-dev-store">
       <NavBar />
-    </>
+    </Router>
   );
 };
 
